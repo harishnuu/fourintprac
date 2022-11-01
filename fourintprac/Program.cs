ec11e1ad1dbe02b17eefdc9248179dc0f3082f64
HP <HP@BOTNIMOUS>
2022-11-01 23:14:49 +0530

    Add project files.

---
+++ fourintprac/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+namespace practical1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no1, no2, no3, no4, product;
+            Console.WriteLine("Enter Four No : ");
+            no1 = Convert.ToInt32(Console.ReadLine());
+            no2 = Convert.ToInt32(Console.ReadLine());
+            no3 = Convert.ToInt32(Console.ReadLine());
+            no4 = Convert.ToInt32(Console.ReadLine());
+            product = no1 * no2 * no3 * no4;
+            Console.WriteLine("Product of Given No = " + product);
+            Console.ReadLine();
+        }
+    }
+}
